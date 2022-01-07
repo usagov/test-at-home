56af10dcf5453e50de1bf67aa65ad761fd6196af
Christa Hartsock <christa.hartsock@gsa.gov>
2022-01-07 14:56:55 -0800

    Update request form and underlying data model
    To match latest mocks

--- spec/models/kit_request_spec.rb
+++ spec/models/kit_request_spec.rb
@@ -1,34 +1,34 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe KitRequest, type: :model do
-  it "has a valid factory" do
+  it 'has a valid factory' do
     expect(FactoryBot.build(:kit_request)).to be_valid
   end
 
-  describe "validations" do
-    it "requires first_name" do
+  describe 'validations' do
+    it 'requires first_name' do
       expect(FactoryBot.build(:kit_request, first_name: nil)).to_not be_valid
-      expect(FactoryBot.build(:kit_request, first_name: "Test")).to be_valid
+      expect(FactoryBot.build(:kit_request, first_name: 'Test')).to be_valid
     end
 
-    it "requires last_name" do
+    it 'requires last_name' do
       expect(FactoryBot.build(:kit_request, last_name: nil)).to_not be_valid
-      expect(FactoryBot.build(:kit_request, last_name: "McTester")).to be_valid
+      expect(FactoryBot.build(:kit_request, last_name: 'McTester')).to be_valid
     end
 
-    it "requires mailing address 1" do
+    it 'requires mailing address 1' do
       expect(FactoryBot.build(:kit_request, mailing_address_1: nil)).to_not be_valid
-      expect(FactoryBot.build(:kit_request, mailing_address_1: "1234 Fake St")).to be_valid
+      expect(FactoryBot.build(:kit_request, mailing_address_1: '1234 Fake St')).to be_valid
     end
 
-    it "requires zip code" do
+    it 'requires zip code' do
       expect(FactoryBot.build(:kit_request, zip_code: nil)).to_not be_valid
-      expect(FactoryBot.build(:kit_request, zip_code: "12345")).to be_valid
+      expect(FactoryBot.build(:kit_request, zip_code: '12345')).to be_valid
     end
 
-    it "requires state" do
+    it 'requires state' do
       expect(FactoryBot.build(:kit_request, state: nil)).to_not be_valid
-      expect(FactoryBot.build(:kit_request, state: "OH")).to be_valid
+      expect(FactoryBot.build(:kit_request, state: 'OH')).to be_valid
     end
   end
 end
--- spec/requests/kit_requests_spec.rb
+++ spec/requests/kit_requests_spec.rb
@@ -1,63 +1,63 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "KitRequests", type: :request do
-  describe "GET /" do
-    it "renders the new template" do
-      get "/"
+RSpec.describe 'KitRequests', type: :request do
+  describe 'GET /' do
+    it 'renders the new template' do
+      get '/'
       expect(response).to have_http_status(200)
       expect(response).to render_template(:new)
     end
   end
 
-  describe "POST /kit_requests" do
-    context "when data is valid" do
+  describe 'POST /kit_requests' do
+    context 'when data is valid' do
       let(:valid_params) do
         {
-          first_name: "Test",
-          last_name: "McTester",
-          email: "foo@example.com",
-          mailing_address_1: "1234 Fake St",
-          mailing_address_2: "Apt A",
-          city: "SF",
-          state: "CA",
-          zip_code: "12345"
+          first_name: 'Test',
+          last_name: 'McTester',
+          email: 'foo@example.com',
+          mailing_address_1: '1234 Fake St',
+          mailing_address_2: 'Apt A',
+          city: 'SF',
+          state: 'CA',
+          zip_code: '12345'
         }
       end
 
-      it "renders the confirmation page" do
-        post "/kit_requests", params: {kit_request: valid_params}
+      it 'renders the confirmation page' do
+        post '/kit_requests', params: { kit_request: valid_params }
 
         expect(response).to have_http_status(200)
         expect(response).to render_template(:confirmation)
       end
 
-      it "creates a new kit request with correct attributes" do
-        expect {
-          post "/kit_requests", params: {kit_request: valid_params}
-        }.to change { KitRequest.count }.by(1)
+      it 'creates a new kit request with correct attributes' do
+        expect do
+          post '/kit_requests', params: { kit_request: valid_params }
+        end.to change { KitRequest.count }.by(1)
 
         record = KitRequest.last
 
-        expect(record.first_name).to eq("Test")
-        expect(record.last_name).to eq("McTester")
-        expect(record.email).to eq("foo@example.com")
-        expect(record.mailing_address_1).to eq("1234 Fake St")
-        expect(record.mailing_address_2).to eq("Apt A")
-        expect(record.city).to eq("SF")
-        expect(record.state).to eq("CA")
-        expect(record.zip_code).to eq("12345")
+        expect(record.first_name).to eq('Test')
+        expect(record.last_name).to eq('McTester')
+        expect(record.email).to eq('foo@example.com')
+        expect(record.mailing_address_1).to eq('1234 Fake St')
+        expect(record.mailing_address_2).to eq('Apt A')
+        expect(record.city).to eq('SF')
+        expect(record.state).to eq('CA')
+        expect(record.zip_code).to eq('12345')
       end
     end
 
-    context "when data is invalid" do
-      it "renders errors without creating new record" do
+    context 'when data is invalid' do
+      it 'renders errors without creating new record' do
         kit_request_params = {
-          first_name: ""
+          first_name: ''
         }
 
-        expect {
-          post "/kit_requests", params: {kit_request: kit_request_params}
-        }.to change { KitRequest.count }.by(0)
+        expect do
+          post '/kit_requests', params: { kit_request: kit_request_params }
+        end.to change { KitRequest.count }.by(0)
 
         expect(response).to have_http_status(200)
         expect(response).to render_template(:new)
@@ -65,9 +65,9 @@ RSpec.describe "KitRequests", type: :request do
     end
   end
 
-  describe "GET /kit_requests" do
-    it "renders a confirmation page" do
-      get "/kit_requests"
+  describe 'GET /kit_requests' do
+    it 'renders a confirmation page' do
+      get '/kit_requests'
       expect(response).to have_http_status(200)
       expect(response).to render_template(:confirmation)
     end
