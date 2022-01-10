5c28fc5774896b7ee23b263d42d9ade6b2262f5f
Christa Hartsock <christa.hartsock@gsa.gov>
2022-01-10 09:50:00 -0800

    Add mailing address 2 to encrypted attributes

--- app/models/kit_request.rb
+++ app/models/kit_request.rb
@@ -1,5 +1,5 @@
 class KitRequest < ApplicationRecord
-  encrypts :first_name, :last_name, :email, :mailing_address_1
+  encrypts :first_name, :last_name, :email, :mailing_address_1, :mailing_address_2
 
   validates_presence_of :first_name, :last_name, :mailing_address_1, :state, :zip_code
 end
