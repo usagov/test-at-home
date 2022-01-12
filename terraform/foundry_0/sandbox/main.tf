a92934ecb70a959acb67131315c3eef46042864c
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-12 13:41:15 -0500

    Stand up multiple dbs in sandbox space

--- terraform/foundry_0/sandbox/main.tf
+++ terraform/foundry_0/sandbox/main.tf
@@ -5,9 +5,8 @@ module "database-primary" {
   cf_api_url       = "https://api.fr.cloud.gov"
   cf_user          = var.cf_user
   cf_password      = var.cf_password
-  cf_space_name    = "ryan.ahearn"
-  cf_org_name      = "sandbox-gsa"
-  env              = "stage"
+  cf_space_name    = "tah-sandbox"
+  env              = "stage-primary"
   recursive_delete = true
   rds_plan_name    = "micro-psql"
 }
@@ -19,8 +18,7 @@ module "database-shard-1" {
   cf_api_url       = "https://api.fr.cloud.gov"
   cf_user          = var.cf_user
   cf_password      = var.cf_password
-  cf_space_name    = "ryan.ahearn"
-  cf_org_name      = "sandbox-gsa"
+  cf_space_name    = "tah-sandbox"
   env              = "stage-shard-1"
   recursive_delete = true
   rds_plan_name    = "micro-psql"
