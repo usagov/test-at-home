bb022424b8fd3db7125c1a708738060d3a8c445f
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-12 14:07:48 -0500

    Add helper for switching between cloud.gov foundations

---
+++ terraform/switch_foundation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# switch the foundation we're logged into
+#
+# usage: ./switch_foundation.sh [0-4]
+# arg is number of the foundation, matching the terraform folders
+
+case $1 in
+  1)
+    hostname="api.fr.wb.cloud.gov"
+    ;;
+  *)
+    hostname="api.fr.cloud.gov"
+    ;;
+esac
+
+cf login -a $hostname --sso
