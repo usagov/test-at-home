d17649e5c1ad13b8132f2777f2a6be335b35aa35
Christa Hartsock <christa.hartsock@gsa.gov>
2022-01-12 21:16:46 -0800

    Restrict routes to english and spanish
    Sicnce we don't yet have Chinese
    
    Also:
    * Converts placehodlers to "TODO" to avoid accidental HTMLification
    * Corrects broken translation in label for mailing address 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+  scope "(:locale)", locale: /#{'en|es'}/ do
     resources :kit_requests, only: %i[create]
     get "kit_requests", to: "kit_requests#confirmation", as: :kit_request_confirmation
 
