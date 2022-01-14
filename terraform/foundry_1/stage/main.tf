c82f721c0816779fc83babbfcf86c0b4427ddf19
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-14 12:53:21 -0500

    Move CDN to foundry_3

--- terraform/foundry_1/stage/main.tf
+++ terraform/foundry_1/stage/main.tf
@@ -1,10 +1,8 @@
 locals {
-  cf_org_name         = "gsa-tts-test-kits"
-  cf_space_name       = "staging"
-  env                 = "stage"
-  recursive_delete    = true
-  global_domain_name  = "staging-covidtest.usa.gov"
-  regional_route_name = "route.staging-covidtest.usa.gov"
+  cf_org_name      = "gsa-tts-test-kits"
+  cf_space_name    = "staging"
+  env              = "stage"
+  recursive_delete = true
 }
 
 module "database" {
@@ -34,28 +32,7 @@ module "domain" {
   cf_space_name          = local.cf_space_name
   env                    = local.env
   recursive_delete       = local.recursive_delete
-  global_domain_name     = local.global_domain_name
-  regional_route_name    = local.regional_route_name
+  global_domain_name     = "staging-covidtest.usa.gov"
+  regional_route_name    = "route.staging-covidtest.usa.gov"
   foundation_domain_name = "westb.staging-covidtest.usa.gov"
 }
-
-#######
-# Set up CDN, only exists in this foundation
-#######
-
-# data "cloudfoundry_space" "space" {
-#   org_name = local.cf_org_name
-#   name     = local.cf_space_name
-# }
-#
-# data "cloudfoundry_service" "external_domain" {
-#   name = "external-domain"
-# }
-#
-# resource "cloudfoundry_service_instance" "cdn_instance" {
-#   name             = "cdn-${local.env}"
-#   space            = data.cloudfoundry_space.space.id
-#   service_plan     = data.cloudfoundry_service.external_domain.service_plans["domain-with-cdn"]
-#   recursive_delete = local.recursive_delete
-#   json_params      = "{\"domains\": \"${local.global_domain_name}\", \"origin\": \"${local.regional_route_name}\"}"
-# }
