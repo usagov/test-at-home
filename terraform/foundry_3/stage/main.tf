d4e5c7f739bd4e1600ca9ef71a65e1eb9c0a5ae1
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-14 13:17:04 -0500

    Created CDN in easta

--- terraform/foundry_3/stage/main.tf
+++ terraform/foundry_3/stage/main.tf
@@ -5,37 +5,37 @@ locals {
   recursive_delete = true
 }
 
-module "database" {
-  source = "../../shared/database"
-
-  aws_region       = var.aws_region
-  cf_api_url       = var.cf_api_url
-  cf_user          = var.cf_user
-  cf_password      = var.cf_password
-  cf_org_name      = local.cf_org_name
-  cf_space_name    = local.cf_space_name
-  env              = local.env
-  recursive_delete = local.recursive_delete
-  rds_plan_name    = "xlarge-gp-psql-redundant"
-}
+# module "database" {
+#   source = "../../shared/database"
+#
+#   aws_region       = var.aws_region
+#   cf_api_url       = var.cf_api_url
+#   cf_user          = var.cf_user
+#   cf_password      = var.cf_password
+#   cf_org_name      = local.cf_org_name
+#   cf_space_name    = local.cf_space_name
+#   env              = local.env
+#   recursive_delete = local.recursive_delete
+#   rds_plan_name    = "xlarge-gp-psql-redundant"
+# }
 
 #######
 # Set up CDN, only exists in this foundation
 #######
 
-# data "cloudfoundry_space" "space" {
-#   org_name = local.cf_org_name
-#   name     = local.cf_space_name
-# }
-#
-# data "cloudfoundry_service" "external_domain" {
-#   name = "external-domain"
-# }
-#
-# resource "cloudfoundry_service_instance" "cdn_instance" {
-#   name             = "cdn-${local.env}"
-#   space            = data.cloudfoundry_space.space.id
-#   service_plan     = data.cloudfoundry_service.external_domain.service_plans["domain-with-cdn"]
-#   recursive_delete = local.recursive_delete
-#   json_params      = "{\"domains\": \"staging-covidtest.usa.gov\", \"origin\": \"route.staging-covidtest.usa.gov\"}"
-# }
+data "cloudfoundry_space" "space" {
+  org_name = local.cf_org_name
+  name     = local.cf_space_name
+}
+
+data "cloudfoundry_service" "external_domain" {
+  name = "external-domain"
+}
+
+resource "cloudfoundry_service_instance" "cdn_instance" {
+  name             = "cdn-${local.env}"
+  space            = data.cloudfoundry_space.space.id
+  service_plan     = data.cloudfoundry_service.external_domain.service_plans["domain-with-cdn"]
+  recursive_delete = local.recursive_delete
+  json_params      = "{\"domains\": \"staging-covidtest.usa.gov\", \"origin\": \"route.staging-covidtest.usa.gov\"}"
+}
