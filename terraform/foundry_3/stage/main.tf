a947d7a3c5864abc4da0ef97ed259e5ce01611f2
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-14 12:58:58 -0500

    Copy over cdn code from foundry_1

--- terraform/foundry_3/stage/main.tf
+++ terraform/foundry_3/stage/main.tf
@@ -1,13 +1,41 @@
-module "database" {
-  source = "../../shared/database"
-
-  aws_region       = "us-gov-east-1"
-  cf_api_url       = "https://api.fr.ea.cloud.gov"
-  cf_user          = var.cf_user
-  cf_password      = var.cf_password
+locals {
   cf_org_name      = "gsa-tts-test-kits"
   cf_space_name    = "staging"
   env              = "stage"
   recursive_delete = true
+}
+
+module "database" {
+  source = "../../shared/database"
+
+  aws_region       = var.aws_region
+  cf_api_url       = var.cf_api_url
+  cf_user          = var.cf_user
+  cf_password      = var.cf_password
+  cf_org_name      = local.cf_org_name
+  cf_space_name    = local.cf_space_name
+  env              = local.env
+  recursive_delete = local.recursive_delete
   rds_plan_name    = "xlarge-gp-psql-redundant"
 }
+
+#######
+# Set up CDN, only exists in this foundation
+#######
+
+# data "cloudfoundry_space" "space" {
+#   org_name = local.cf_org_name
+#   name     = local.cf_space_name
+# }
+#
+# data "cloudfoundry_service" "external_domain" {
+#   name = "external-domain"
+# }
+#
+# resource "cloudfoundry_service_instance" "cdn_instance" {
+#   name             = "cdn-${local.env}"
+#   space            = data.cloudfoundry_space.space.id
+#   service_plan     = data.cloudfoundry_service.external_domain.service_plans["domain-with-cdn"]
+#   recursive_delete = local.recursive_delete
+#   json_params      = "{\"domains\": \"staging-covidtest.usa.gov\", \"origin\": \"route.staging-covidtest.usa.gov\"}"
+# }
