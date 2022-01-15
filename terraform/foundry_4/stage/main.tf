75e47efd7158475a52f525f6fea7c3c69c3ca75e
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-15 12:07:17 -0500

    Enable domain service in foundry 2 & 4

--- terraform/foundry_4/stage/main.tf
+++ terraform/foundry_4/stage/main.tf
@@ -23,19 +23,19 @@ module "database" {
 
 # The following lines need to be commented out for the initial `terraform apply`
 # and then re-enabled and applied after the app has first been deployed
-# module "domain" {
-#   source = "../../shared/domain"
-#
-#   aws_region             = local.aws_region
-#   cf_api_url             = local.cf_api_url
-#   cf_user                = var.cf_user
-#   cf_password            = var.cf_password
-#   cf_org_name            = local.cf_org_name
-#   cf_space_name          = local.cf_space_name
-#   env                    = local.env
-#   recursive_delete       = local.recursive_delete
-#   global_domain_name     = "staging-covidtest.usa.gov"
-#   origin_domain_name     = "route.staging-covidtest.usa.gov"
-#   regional_domain_name   = "east.staging-covidtest.usa.gov"
-#   foundation_domain_name = "eastb.staging-covidtest.usa.gov"
-# }
+module "domain" {
+  source = "../../shared/domain"
+
+  aws_region             = local.aws_region
+  cf_api_url             = local.cf_api_url
+  cf_user                = var.cf_user
+  cf_password            = var.cf_password
+  cf_org_name            = local.cf_org_name
+  cf_space_name          = local.cf_space_name
+  env                    = local.env
+  recursive_delete       = local.recursive_delete
+  global_domain_name     = "staging-covidtest.usa.gov"
+  origin_domain_name     = "route.staging-covidtest.usa.gov"
+  regional_domain_name   = "east.staging-covidtest.usa.gov"
+  foundation_domain_name = "eastb.staging-covidtest.usa.gov"
+}
