b5e5f18b9786a64077270b0e59580217654334c9
Christa Hartsock <christa.hartsock@gsa.gov>
2022-01-15 08:40:05 -0800

    Limit email to less than 50 characters
    To match requirements of USPS system

--- app/javascript/helpers/validate.js
+++ app/javascript/helpers/validate.js
@@ -11,10 +11,16 @@ export const validate = new Bouncer("#form", {
       if (!field.getAttribute("data-bouncer-is-valid-zip")) return false;
 
       return !/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(field.value);
+    },
+    emailLength: field => {
+      if (!field.getAttribute("data-bouncer-email-length")) return false;
+
+      return field.value.length >= 50;
     }
   },
   messages: {
     isValidZip: I18n.t("js.invalid.zip_code"),
+    emailLength: I18n.t("js.invalid.email_too_long"),
     missingValue: {
       select: I18n.t("js.invalid.missing_select"),
       default: I18n.t("js.invalid.missing_default")
