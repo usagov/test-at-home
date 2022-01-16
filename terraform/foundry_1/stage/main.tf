40dd56d3a9f51bde00d037be9b272e5185dc3d99
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-16 08:32:17 -0500

    Remove old single-db modules from foundries 1, 2, 4

--- terraform/foundry_1/stage/main.tf
+++ terraform/foundry_1/stage/main.tf
@@ -7,20 +7,6 @@ locals {
   recursive_delete = true
 }
 
-module "database" {
-  source = "../../shared/database"
-
-  aws_region       = local.aws_region
-  cf_api_url       = local.cf_api_url
-  cf_user          = var.cf_user
-  cf_password      = var.cf_password
-  cf_org_name      = local.cf_org_name
-  cf_space_name    = local.cf_space_name
-  env              = local.env
-  recursive_delete = local.recursive_delete
-  rds_plan_name    = "xlarge-gp-psql-redundant"
-}
-
 module "database-n" {
   source = "../../shared/database-n"
 
