b9548ac156e152310e0bedee7bcd4ac2c07bc680
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-16 14:54:10 -0500

    Foundry 4 prod terraform db

---
+++ terraform/foundry_4/prod/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    cloudfoundry = {
+      source  = "cloudfoundry-community/cloudfoundry"
+      version = "0.12.6"
+    }
+    aws = {
+      version = "~> 3.11.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "cg-f6858d77-30ab-4017-a0d0-1ce0f2929458"
+    key     = "terraform.tfstate.prod"
+    encrypt = "true"
+    region  = "us-gov-east-1"
+    profile = "tah-foundry-4-backend"
+  }
+}
