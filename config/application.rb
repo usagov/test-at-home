96f1f1f5519da9eee831fe9e4a1bb4b474c130fd
Brian Hurst <brian.hurst@gsa.gov>
2022-01-16 17:42:10 -0500

    Cache error pages

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,9 @@ module TestAtHome
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.exceptions_app = self.routes
+    if Rails.env.production?
+      config.exceptions_app = self.routes
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
