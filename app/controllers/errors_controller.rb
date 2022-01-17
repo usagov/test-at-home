f2926e3275665ee1db8cf90f03eb066bbbf1e0b5
Mitchell Henke <mitchell.henke@gsa.gov>
2022-01-17 13:08:25 -0600

    return 404/500 status codes on cached pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -12,7 +12,7 @@ class ErrorsController < ApplicationController
   def render_not_found
     filename = Rails.root.join("tmp/cache/not_found_#{I18n.locale}.html")
     if File.exist?(filename)
-      render file: filename, layout: false
+      render file: filename, layout: false, status: 404
     else
       html = render_to_string :not_found
       if ENV["CACHE_ERRORS"] == "true"
@@ -27,7 +27,7 @@ class ErrorsController < ApplicationController
   def render_internal_server_error
     filename = Rails.root.join("tmp/cache/internal_server_error_#{I18n.locale}.html")
     if File.exist?(filename)
-      render file: filename, layout: false
+      render file: filename, layout: false, status: 500
     else
       html = render_to_string :internal_server_error
       if ENV["CACHE_ERRORS"] == "true"
