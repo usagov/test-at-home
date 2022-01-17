77d7142d6fbe65f74974f12145ebfd9c8480cf71
Brian Hurst <brian.hurst@gsa.gov>
2022-01-16 23:09:24 -0500

    Add IE polyfill

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Entry point for the build script in your package.json
 import "uswds";
 import i18n from "i18n-js";
+import "promise-polyfill/src/polyfill";
 
 import { autocomplete } from "./helpers/addressAutocomplete";
 import { verifyAddress } from "./helpers/addressVerification";
