493d1b1d873f8a3a3d63f05a3268d86509d79150
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-01-20 11:00:52 -0500

    Fix errors controller tests

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "Errors", type: :request do
   describe "GET /not_found" do
     it "returns http success" do
       get "/404"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:not_found)
     end
   end
 
   describe "GET /internal_server_error" do
     it "returns http success" do
       get "/500"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:internal_server_error)
     end
   end
 end
